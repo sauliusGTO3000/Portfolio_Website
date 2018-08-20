de3ee5f04cac5d1951b64a6362a443e96a7db501
sauliusgto3000 <sauliusgto3000@gmail.com>
2018-08-20 15:44:02 +0300

    added front page animations

---
+++ my_project/assets/js/app.js
@@ -0,0 +1,21 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you require will output into a single css file (app.scss in this case)
+require('../css/app.scss');
+
+// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
+// var $ = require('jquery');
+
+
+console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+$('.bodyWrapper').css({ height: window.innerHeight });
+
+$(window).resize(function () {
+    $('.bodyWrapper').css({ height: window.innerHeight });
+});
+
---
+++ my_project/webpack.config.js
@@ -0,0 +1,36 @@
+var Encore = require('@symfony/webpack-encore');
+
+Encore
+    // directory where compiled assets will be stored
+    .setOutputPath('public/build/')
+    // public path used by the web server to access the output path
+    .setPublicPath('/build')
+    .cleanupOutputBeforeBuild()
+    .enableSourceMaps(!Encore.isProduction())
+    // only needed for CDN's or sub-directory deploy
+    //.setManifestKeyPrefix('build/')
+
+    /*
+     * ENTRY CONFIG
+     *
+     * Add 1 entry for each "page" of your app
+     * (including one that's included on every page - e.g. "app")
+     *
+     * Each entry will result in one JavaScript file (e.g. app.js)
+     * and one CSS file (e.g. app.scss) if you JavaScript imports CSS.
+     */
+    .addEntry('js/app', './assets/js/app.js')
+    .addStyleEntry('css/app', './assets/css/app.scss')
+    .addStyleEntry('css/homepage', './assets/css/homepage.scss')
+    //.addEntry('page1', './assets/js/page1.js')
+    //.addEntry('page2', './assets/js/page2.js')
+
+
+    // uncomment if you use Sass/SCSS files
+    .enableSassLoader()
+
+    // uncomment for legacy applications that require $/jQuery as a global variable
+    .autoProvidejQuery()
+;
+
+module.exports = Encore.getWebpackConfig();

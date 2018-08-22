70889d0872ac5ef43a9a3d71f8f055fd605660ab
sauliusgto3000 <sauliusgto3000@gmail.com>
2018-08-23 01:01:39 +0300

    testing new solution to hero section

--- my_project/assets/js/app.js
+++ my_project/assets/js/app.js
@@ -10,6 +10,38 @@ require('../css/app.scss');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // var $ = require('jquery');
+// $(document).ready(function() {
+//     $('.bodyWrapper').css("height", window.innerHeight);
+// });
 
-$('.bodyWrapper').css("height",window.innerHeight) ;
+if ("ontouchstart" in document.documentElement) {
+    document.body.classList.add('touch-device');
 
+} else {
+    document.body.classList.add('hover-device');
+}
+
+
+if (jQuery('body').hasClass("touch-device")) {
+//Loading height on touch-device
+    function calcFullHeight() {
+        jQuery('.hompageTop').css("height", $(window).height());
+    }
+
+    (function($) {
+        calcFullHeight();
+
+        jQuery(window).on('orientationchange', function() {
+            // 500ms timeout for getting the correct height after orientation change
+            setTimeout(function() {
+                calcFullHeight();
+            }, 500);
+
+        });
+    })(jQuery);
+
+} else {
+    jQuery('.hompageTop').css("height", "100vh");
+
+
+}
